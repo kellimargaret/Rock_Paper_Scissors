3b98cccdcedefdfb610b4d9b68eb6d94242ca1fa
Chris <ludichrislyts@gmail.com>
2015-08-12 11:53:32 -0700

    add method to pass test 3

--- src/RockPaperScissor.php
+++ src/RockPaperScissor.php
@@ -5,10 +5,13 @@
         {
             if ($player1 === $player2){
                 $result = "tie";
+            // Player 1 == Rock Action
             } else {
                 if ($player1 === "rock") {
                     if ($player2 === "paper"){
                         $result = "Player Two Wins";
+                    } else {
+                        $result = "Player One Wins";
                     }
                 }
             }
