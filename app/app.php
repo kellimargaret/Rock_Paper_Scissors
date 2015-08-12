08fc694ce4a11eac7aae30baed1754dae32dc621
Chris <ludichrislyts@gmail.com>
2015-08-12 14:14:51 -0700

    create home page

--- app/app.php
+++ app/app.php
@@ -4,12 +4,14 @@
 
     $app = new Silex\Application();
 
-    $app->register(new Silex\Provide\TwigServiceProvider(), array(
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use($app) {
             return $app['twig']->render('player_one_form.html.twig');
     });
+
+    return $app;
 
 ?>
