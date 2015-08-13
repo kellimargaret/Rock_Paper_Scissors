447806e100ce51517e1c3d451a380268d00c73a8
Kelli <kelli.sommerdyke@gmail.com>
2015-08-13 16:08:10 -0700

    Add construct functions with save, delete, and get all

--- app/app.php
+++ app/app.php
@@ -2,7 +2,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RockPaperScissor.php";
 
+    //initialize $_SESSION array to hold results
     session_start();
+    if(empty($_SESSION['list_of_moves'])) {
+        $_SESSION['list_of_moves'] = array();
+    }
 
     $app = new Silex\Application();
 
@@ -10,29 +14,22 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    //initialize $_SESSION array to hold results
-    if (empty($_SESSION['results_array']))
-    {
-        $_SESSION['results_array'] = array();
-    }
-
     $app->get("/", function() use($app) {
-
-
-        var_dump($_SESSION['results_array']);
+        RockPaperScissor::deleteAll();
         return $app['twig']->render('player_one_form.html.twig');
     });
 
     $app->get("/player_two_form", function() use ($app){
-        array_push($_SESSION['results_array'], $_GET['player1']);
+        $player1_input = new RockPaperScissor($_GET['player1']);
+        $player1_input->save();
         return $app['twig']->render('player_two_form.html.twig');
     });
 
     $app->get("/gameover", function() use ($app){
-        $new_game = new RockPaperScissor;
-        array_push($_SESSION['results_array'], $_GET['player2']);
-        $game_results = $new_game->play_rock($_SESSION['results_array'][0], $_SESSION['results_array'][1]);
-        return $app['twig']->render('results.html.twig', array('result' => $game_results));
+        $new_game = RockPaperScissor::getAll()[0];
+        $new_game->setPlayer2($_GET['player2']);
+        $game_results = $new_game->play_rock($new_game->getPlayer1(), $new_game->getPlayer2());
+        return $app['twig']->render('results.html.twig', array('result' => $game_results, 'player1' => $new_game->getPlayer1(), 'player2' => $_GET['player2']));
     });
 
     return $app;
--- src/RockPaperScissor.php
+++ src/RockPaperScissor.php
@@ -1,6 +1,36 @@
 <?php
     class RockPaperScissor
     {
+
+        private $player1;
+        private $player2;
+
+        function __construct($player1="", $player2="")
+        {
+            $this->player1 = $player1;
+            $this->player2 = $player2;
+        }
+
+        function setPlayer1($new_player1)
+        {
+            $this->player1 = $new_player1;
+        }
+
+        function getPlayer1()
+        {
+            return $this->player1;
+        }
+
+        function setPlayer2($new_player2)
+        {
+            $this->player2 = $new_player2;
+        }
+
+        function getPlayer2()
+        {
+            return $this->player2;
+        }
+
         function play_rock($player1, $player2)
         {
             if ($player1 === $player2){
@@ -35,17 +65,19 @@
 
             return $result;
         }
-        // function save()
-        // {
-        //     array_push($_SESSION['results_array'], $this);
-        // }
-        // static function getAll()
-        // {
-        //     return $_SESSION['results_array'];
-        // }
-        // static function deleteAll()
-        // {
-        //     $_SESSION['results_array'] = array();
-        // }
+        function save()
+        {
+            array_push($_SESSION['list_of_moves'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_moves'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_moves'] = array();
+        }
     }
 ?>
